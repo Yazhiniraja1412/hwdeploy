b00ee343b253f34839814d0d735040916dbf05f6
Yazhini <yazhiniraja1412@gmail.com>
2025-07-07 20:45:27 +0530

    first commit

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { Home } from "./home/home";
+const routes: Routes = [
+  {path:'',component:Home}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
